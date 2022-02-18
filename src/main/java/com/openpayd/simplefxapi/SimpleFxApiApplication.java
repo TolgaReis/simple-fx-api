227e671612067e4162cfb48e180a8c89135e8ede
TolgaReis <tolgaareis@gmail.com>
2022-02-18 18:54:07 +0300

    Create Project

---
+++ src/main/java/com/openpayd/simplefxapi/SimpleFxApiApplication.java
@@ -0,0 +1,13 @@
+package com.openpayd.simplefxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFxApiApplication.class, args);
+	}
+
+}
